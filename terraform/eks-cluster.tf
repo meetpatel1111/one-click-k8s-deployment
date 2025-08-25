7bb9a3d9104ddd2f3dc7ae5cc7166c04e8840a45
Meetkumar Patel <pmeet464@gmail.com>
2025-08-25 18:19:17 +0530

    Added cluster role and node-group role

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -1,9 +1,69 @@
+resource "aws_iam_role" "eks_role" {
+  name = "${var.cluster_name}-eks-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "eks.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  role       = aws_iam_role.eks_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_iam_role" "eks_nodes" {
+  name = "${var.cluster_name}-nodegroup-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_worker_node" {
+  role       = aws_iam_role.eks_nodes.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
+  role       = aws_iam_role.eks_nodes.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_registry_policy" {
+  role       = aws_iam_role.eks_nodes.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+
 resource "aws_eks_cluster" "eks" {
   name     = var.cluster_name
   role_arn = aws_iam_role.eks_role.arn
 
-  vpc_config { 
-    subnet_ids = aws_subnet.private[*].id
+  vpc_config {
+    subnet_ids = aws_subnet.public[*].id
+    endpoint_private_access = true
+    endpoint_public_access  = true
   }
-  enabled_cluster_log_types = ["api","audit","authenticator"]
+
+  tags = {
+    Name = var.cluster_name
+  }
 }
