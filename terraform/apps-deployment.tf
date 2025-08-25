07c28571c7cf12e06e3d95eb181a922d31d7a099
Meetkumar Patel <86229515+meetpatel1111@users.noreply.github.com>
2025-08-25 17:54:04 +0530

    Fixed unexpected semicolon issues

--- terraform/apps-deployment.tf
+++ terraform/apps-deployment.tf
@@ -2,7 +2,11 @@ resource "kubernetes_deployment" "nginx" {
   metadata { name = "nginx" }
   spec {
     replicas = var.nginx_replicas
-    selector { match_labels = { app = "nginx" } }
+    selector { 
+      match_labels = { 
+        app = "nginx" 
+        } 
+      }
     template {
       metadata { 
         labels = { 
@@ -11,12 +15,12 @@ resource "kubernetes_deployment" "nginx" {
       }
       spec {
         container { 
-          name="nginx"; 
-          image="nginx:latest"; 
+          name="nginx"
+          image="nginx:latest"
           ports {
             container_port=80
-            } 
-          }
+          } 
+        }
       }
     }
   }
@@ -41,8 +45,8 @@ resource "kubernetes_deployment" "nodejs" {
       }
       spec {
         container { 
-          name="nodejs-app"; 
-          image=var.nodejs_docker_image; 
+          name="nodejs-app"
+          image=var.nodejs_docker_image
           ports{container_port=3000
           } 
         }
@@ -52,10 +56,17 @@ resource "kubernetes_deployment" "nodejs" {
 }
 
 resource "kubernetes_deployment" "k8sgpt" {
-  metadata { name="k8sgpt"; namespace="k8sgpt" }
+  metadata { 
+    name="k8sgpt"
+    namespace="k8sgpt"
+  }
   spec {
     replicas = var.k8sgpt_replicas
-    selector { match_labels={app="k8sgpt"} }
+    selector { 
+      match_labels= {
+        app="k8sgpt"
+      } 
+    }
     template {
       metadata { 
         labels=
@@ -65,8 +76,8 @@ resource "kubernetes_deployment" "k8sgpt" {
       }
       spec {
         container { 
-          name="k8sgpt"; 
-          image="ghcr.io/k8sgpt-ai/k8sgpt:latest"; 
+          name="k8sgpt"
+          image="ghcr.io/k8sgpt-ai/k8sgpt:latest"
           ports {
             container_port=80
           } 
