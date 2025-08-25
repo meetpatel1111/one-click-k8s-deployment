4fcfa9f15d9f3ab75c134dc322136bd22c99241c
Meetkumar Patel <pmeet464@gmail.com>
2025-08-25 19:18:43 +0530

    Fixed backed initialization issue

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,3 @@
 terraform {
-  backend "s3" {
-    bucket         = "my-terraform-backend-${var.environment}"
-    key            = "state/terraform.tfstate"                 
-    region         = "us-east-1"                           
-    dynamodb_table = "terraform-locks-${var.environment}"      
-    encrypt        = true
-  }
+  backend "s3" {}
 }
